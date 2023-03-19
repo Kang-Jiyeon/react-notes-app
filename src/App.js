8e2cb951c6c0d739e6108844ce7f1cf944a3fb8c
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-03-20 04:10:57 +0900

    230320 update
    [추가] AddNote 컴포넌트 추가(새로운 메모) / 글자 제한 기능/ 메모 삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,26 +3,47 @@ import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 function App() {
-  const [notes, setNote] = useState([
-    {
-      id: nanoid(),
-      text: "This is my first note!",
-      date: "19/03/2022",
-    },
-    {
-      id: nanoid(),
-      text: "This is my second note!",
-      date: "19/03/2022",
-    },
-    {
-      id: nanoid(),
-      text: "This is my third note!",
-      date: "19/03/2022",
-    },
+  const [notes, setNotes] = useState([
+    // {
+    //   id: nanoid(),
+    //   text: "This is my first note!",
+    //   date: "19/03/2022",
+    // },
+    // {
+    //   id: nanoid(),
+    //   text: "This is my second note!",
+    //   date: "19/03/2022",
+    // },
+    // {
+    //   id: nanoid(),
+    //   text: "This is my third note!",
+    //   date: "19/03/2022",
+    // },
   ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
     <div className="container">
-      <NotesList notes={notes} />
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
   );
 }
